694479b30dbb340a3c430f6a8d9b25c2aad3c499
ajh265 <ajh265@4d31f3d0-ce38-4df3-9f2c-fa11c909a453>
2015-11-10 18:20:03 +0000

    Observer Observable pattern implemented

--- P2/g11_ajh265_kex496/app/src/main/java/com/example/localadmin/g11_ajh265_kex496/rest/RestClientPoller.java
+++ P2/g11_ajh265_kex496/app/src/main/java/com/example/localadmin/g11_ajh265_kex496/rest/RestClientPoller.java
@@ -14,96 +14,32 @@ import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EBean;
 import org.androidannotations.annotations.rest.RestService;
 
+import java.util.Observable;
+
 /**
  * Created by localadmin on 11/9/15.
  */
 @EBean
-public class RestClientPoller {
+public class RestClientPoller extends Observable {
 
-    private long tankId;
+
     private boolean gameRunning = true;
-    private static final String TAG = "TankClientActivity";
-    private GridWrapper grid;
-
-    @Bean
-    protected GridAdapter mGridAdapter;
-
-    @RestService
-    BulletZoneRestClient restClient;
-
-    Context myContext;
-
-    public RestClientPoller( Context c ){
-        myContext = c;
-    }
-
-
-    public long startPolling( View myGridView ){
-
-        joinAsync();
-        SystemClock.sleep(500);
-        getField();
-        SystemClock.sleep(500);
-
-        mGridAdapter.updateList(grid);
-        mGridAdapter.setTankID(tankId);
-
-        ((GridView)myGridView).setAdapter(mGridAdapter);
+    public RestClientPoller( ){
 
         startPoll();
 
-        return tankId;
-
     }
-
-
-    @Background
-    void joinAsync() {
-        try {
-            tankId = restClient.join().getResult();
-            Log.d(TAG, "tankId is " + tankId);
-        } catch (Exception e) {
-
-            Log.d(TAG, "FAILED JOIN");
-        }
-    }
-
-
-
-
-    @Background
-    void getField() {
-        try {
-            grid = restClient.grid();
-
-        } catch (Exception e) {
-
-        }
-    }
-
 
     @Background
     void startPoll() {
         while(gameRunning){
 
-            getField();
             SystemClock.sleep(100);
-            updateDisplay();
+            setChanged();
+            notifyObservers();
 
         }
     }
-
-
-    @org.androidannotations.annotations.UiThread
-    public void updateDisplay(){
-
-        mGridAdapter.updateList(grid);
-        mGridAdapter.notifyDataSetChanged();
-
-    }
-
-
-
 
 
 
